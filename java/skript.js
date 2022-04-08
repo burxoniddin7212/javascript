bdc319efcf95e2ad29339279a3edcb4bdd0ce74e
burxoniddin7212 <liverpul7212@gmail.com>
2022-04-08 23:34:21 +0500

    first commit

---
+++ java/skript.js
@@ -0,0 +1,8 @@
+alert("Xush kelibsiz");
+var firstName = prompt("Ismingiz nma?");
+var familya = prompt("Familyangiz nma?");
+var age = prompt("Yoshingiz nechchida?");
+var job = prompt("Kaspingiz nma?");
+// console.log("Ism: " + firstName + "\n" + "Familya: " + familya + "\n" + "Yosh: " + age + "\n" + "Kasb: " + job);
+console.log(`Ism: ${firstName} \nFamilya: ${familya} \nYosh: ${age} \nKasb: ${job}`);
+
